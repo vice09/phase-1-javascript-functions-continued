d4f10888aae7b37a542d33a792629db3df83de8a
Edwin Victor Oduor Odhiambo <oduoredwin43@gmail.com>
2022-06-12 00:09:38 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(mess= "roller-skate") {
+    return "This Saturday, I want to ${message}!"
+};
+
+function mondayWork(mess= "go to the office") {
+  return "This Monday, I will ${message}."
+};
+let wrapAdjective = function (mess='*') {
+    return function (text='special') {
+        return "You are ${message}${text}${message}!"
+    };
+}
+
